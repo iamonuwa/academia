58b176bda8f509e22cca774a2779b374a597cbbc
Onuwa Nnachi Isaac <matrix4u2002@gmail.com>
2019-11-07 20:42:54 +0100

    Add theme

---
+++ src/constants/index.js
@@ -0,0 +1,8 @@
+export const APP_NAME = "journal";
+export const APP_VERSION = "0.1.0";
+export const SUPPORTED_THEMES = {
+  DARK: "DARK",
+  LIGHT: "LIGHT"
+};
+
+export const SUPPORTED_DOC_FORMATS = ["docx", "pdf"];
