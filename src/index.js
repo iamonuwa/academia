5b2c7fc69c93f41f6f472d0a3328a33998cbf81b
Onuwa Nnachi Isaac <matrix4u2002@gmail.com>
2019-11-07 20:42:01 +0100

    Remove file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { ToastProvider } from 'react-toast-notifications'
+import App from "./pages/App";
+import ThemeProvider, { GlobalStyle } from "./theme";
+import StorageProvider, {
+  Updater as StorageUpdater
+} from "./contexts/LocalStorage.context";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import * as serviceWorker from "./serviceWorker";
+
+const Updaters = () => (
+  <>
+    <StorageUpdater />
+  </>
+);
+
+const ContextProviders = ({ children }) => (
+  <>
+    <StorageProvider>
+      <ToastProvider autoDismissTimeout={6000}>{children}</ToastProvider>
+    </StorageProvider>
+  </>
+);
+
+ReactDOM.render(
+  <ContextProviders>
+    <Updaters />
+    <ThemeProvider>
+      <BrowserRouter>
+        <GlobalStyle />
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
+  </ContextProviders>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
